ae74b5a9a51888ebfd161ade69c8348a0a3a2d2d
msmenard <matthew.s.menard@gmail.com>
2025-09-21 20:15:05 -0400

    Added player, and have drawn the triangle on the screen.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-    game_on = True
-    while game_on:
-        screen.fill("black")
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+        
 
 if __name__ == "__main__":
     main()
