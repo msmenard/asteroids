f81c6c67ef455f86521f27c1310d1b9f441cc74c
msmenard <matthew.s.menard@gmail.com>
2025-09-22 20:33:36 -0400

    Added groups to contain multiple objects.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x= SCREEN_WIDTH / 2, y= SCREEN_HEIGHT / 2)
 
     while True:
@@ -15,9 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
